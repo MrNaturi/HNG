ba3cc731f2a28b92dc37c0b7cd5d015db699ea47
MrNaturi <115954743+MrNaturi@users.noreply.github.com>
2025-02-03 13:10:52 +0100

    functionality completed

--- task-1/script.js
+++ task-1/script.js
@@ -6,7 +6,46 @@ const colors =     [
     "rgb(145, 170, 200)",
     "rgb(108, 122, 98)"
 ]
-console.log(colors[Math.floor(Math.random() * 6)])
-const Game = (function (){
+const colorBox = document.getElementById("colorBox")
+const btnCard = document.getElementById('btnCard')
+const scoreTrack = document.getElementById('scoreTrack')
+const reset = document.getElementById('reset')
+let score = 0
+let randColor
 
+const colorChange = () => {
+    randColor = Math.floor(Math.random() * 6)
+    colorBox.style.backgroundColor = colors[randColor]
+}
+const updateUI = () => {
+    colorChange()
+    scoreTrack.innerText = `Correct! Score: ${score}`
+}
+reset.onclick = () => {
+    colorChange()
+    score = 0
+    scoreTrack.innerText = `Score: ${score}`
+}
+const choice = (inde) => {
+    if (inde === randColor){
+    score++
+    updateUI()
+} else {
+    scoreTrack.innerText = 'Incorrect! Try Again!!'
+}
+}
+
+colors.forEach((_, index) => {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+
+    const button = document.createElement('button');
+    button.className = 'selectors';
+    button.innerText = '';
+    button.style.backgroundColor = colors[index]
+    button.onclick = () => { choice(index) }
+
+    cell.appendChild(button);
+    btnCard.appendChild(cell);
 })
+colorChange()
