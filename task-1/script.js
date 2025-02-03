bad7e1d1441c3b934ed236206749dea35f452b45
MrNaturi <115954743+MrNaturi@users.noreply.github.com>
2025-02-03 13:53:05 +0100

    styling

--- task-1/script.js
+++ task-1/script.js
@@ -1,51 +1,56 @@
-const colors =     [
+const colors = [
     "rgb(242, 143, 130)",
     "rgb(80, 153, 142)",
     "rgb(227, 199, 175)",
     "rgb(139, 100, 133)",
     "rgb(145, 170, 200)",
     "rgb(108, 122, 98)"
-]
-const colorBox = document.getElementById("colorBox")
-const btnCard = document.getElementById('btnCard')
-const scoreTrack = document.getElementById('scoreTrack')
-const reset = document.getElementById('reset')
-let score = 0
-let randColor
+];
+
+const colorBox = document.getElementById("colorBox");
+const btnCard = document.getElementById('btnCard');
+const scoreTrack = document.getElementById('scoreTrack');
+const reset = document.getElementById('reset');
+let score = 0;
+let randColor;
 
 const colorChange = () => {
-    randColor = Math.floor(Math.random() * 6)
-    colorBox.style.backgroundColor = colors[randColor]
-}
+    randColor = Math.floor(Math.random() * 6);
+    colorBox.style.backgroundColor = colors[randColor];
+    colorBox.classList.remove("correct");
+};
+
 const updateUI = () => {
-    colorChange()
-    scoreTrack.innerText = `Correct! Score: ${score}`
-}
+    colorChange();
+    scoreTrack.innerText = `Correct! Score: ${score}`;
+};
+
 reset.onclick = () => {
-    colorChange()
-    score = 0
-    scoreTrack.innerText = `Score: ${score}`
-}
-const choice = (inde) => {
-    if (inde === randColor){
-    score++
-    updateUI()
-} else {
-    scoreTrack.innerText = 'Incorrect! Try Again!!'
-}
-}
+    const userConfirmed = confirm("Are you sure you want to restart?");
+    if (userConfirmed) {
+        colorChange();
+        score = 0;
+        scoreTrack.innerText = `Score: ${score}`;
+    }
+};
+
+const choice = (index) => {
+    if (index === randColor) {
+        score++;
+        updateUI();
+        colorBox.classList.add("correct");
+    } else {
+        scoreTrack.innerText = 'Try Again!!';
+    }
+};
 
 colors.forEach((_, index) => {
-    const cell = document.createElement('div');
-    cell.className = 'cell';
-
     const button = document.createElement('button');
     button.className = 'selectors';
-    button.innerText = '';
-    button.style.backgroundColor = colors[index]
-    button.onclick = () => { choice(index) }
+    button.style.backgroundColor = colors[index];
+    button.onclick = () => { choice(index) };
+    btnCard.appendChild(button);
+});
 
-    cell.appendChild(button);
-    btnCard.appendChild(cell);
-})
-colorChange()
+scoreTrack.innerText = `Score: ${score}`;
+colorChange();
