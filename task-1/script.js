1b93efa0e1996572b5879b2ab0b21b61c97247ac
MrNaturi <115954743+MrNaturi@users.noreply.github.com>
2025-02-03 11:57:15 +0100

    initial commit

---
+++ task-1/script.js
@@ -0,0 +1,12 @@
+const colors =     [
+    "rgb(242, 143, 130)",
+    "rgb(80, 153, 142)",
+    "rgb(227, 199, 175)",
+    "rgb(139, 100, 133)",
+    "rgb(145, 170, 200)",
+    "rgb(108, 122, 98)"
+]
+console.log(colors[Math.floor(Math.random() * 6)])
+const Game = (function (){
+
+})
